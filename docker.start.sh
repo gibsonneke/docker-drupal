087a599666f212765f2b8ffeda176b822918849a
Emmanuel Libbrecht <libbrecht.emmanuel@gmail.com>
2022-02-08 18:15:34 +0100

    Create required assets and db folders if required + execute composer install if required + set permissions on files and private folder if required

--- docker.start.sh
+++ docker.start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [[ ! -e "assets" ]]; 
+then
+  echo "Create the assets folder"
+  mkdir assets
+fi
+
+if [[ ! -e "db" ]]; 
+then
+  echo "Create the db folder"
+  mkdir db
+fi
+
 if [[ ! -f "assets/smtp.json" ]];
 then
   echo "Copy example smtp.json to project root"
@@ -9,4 +21,23 @@ fi
 wait
 
 docker-compose -f docker-compose.yml up -d
+
+wait
+
+if [[ ! -e "vendor" ]]; 
+then
+  echo "Running composer install since this has not been done yet"
+  docker-compose run --rm phpfpm composer install
+fi
+
+if [ "$(stat -c '%a' web/sites/default/files)" != "777" ]
+then
+  chmod -R 777 web/sites/default/files
+fi
+
+if [ "$(stat -c '%a' web/sites/default/private)" != "777" ]
+then
+  chmod -R 777 web/sites/default/private
+fi
+
 docker-compose -f docker-compose.yml ps
